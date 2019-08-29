77febfbf1391e2c1fe8d3d72f43aa962714237e8
Pushtaryk Oleksii <pushtarik@gmail.come>
2019-08-29 11:38:55 +0300

    Updates

---
+++ precache-manifest.d5b98f6db34db0a3153f1adddfce56cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6d4a57f8ccb007194106b944cb060f",
+    "url": "/react-homework1/index.html"
+  },
+  {
+    "revision": "95f09c56a4bff36d39a8",
+    "url": "/react-homework1/static/css/main.fceba357.chunk.css"
+  },
+  {
+    "revision": "906546adc73153a98bb7",
+    "url": "/react-homework1/static/js/2.ec86efed.chunk.js"
+  },
+  {
+    "revision": "95f09c56a4bff36d39a8",
+    "url": "/react-homework1/static/js/main.52225814.chunk.js"
+  },
+  {
+    "revision": "0946144f02c30d99aca3",
+    "url": "/react-homework1/static/js/runtime~main.422a95f7.js"
+  }
+]);
